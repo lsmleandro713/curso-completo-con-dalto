80231b8b5747dfb880762a76fbe3838848ba33d5
Tu Nombre <lsaldarriagam19945@gmail.com>
2024-11-14 21:50:48 -0600

    ejercicios de bucles que saque del chat gpt

---
+++ javascript.js
@@ -0,0 +1,76 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+BUCLE USANDO BREAK
+for(let i = 10; i>=0;i--){
+    document.write(` ${i}<br>`);
+    if(i==0){
+        document.write(` ¡ DESPEGUE !`);
+        break;
+    }
+}
+*/
+
+
+
+
+
+
+/* PROGRAMA QUE PIDE UN NUMERO Y LO MULTIPLICA
+
+
+let multi=0;
+let numero=0;
+let resultado=0;
+
+let op= prompt(` por favor ingrese el numero de la tabla que desea imprimir`);
+
+document.write(` <h1>ESTA ES LA TABLA DEL ${op}</h1> <br> `)
+for (let i = 1; i < 11; i++) {
+    multi = op * i;
+    document.write(` 
+        ${op} * ${i} = ${multi} <br>
+        `);
+    
+
+    
+}
+*/
+
